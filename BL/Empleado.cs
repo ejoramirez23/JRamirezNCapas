a003a716eb6b3a170677509de9dd2ccec1f1ea7e
“Soporte <soporte@bstmexico.com>
2023-12-29 00:06:58 -0600

    Agregar archivos de proyecto.

---
+++ BL/Empleado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BL
+{
+    public class Empleado
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+
+                using (DL.JRamirezNCapasEntities context = new DL.JRamirezNCapasEntities())
+                {
+
+                    
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.Ex = ex;
+                result.Message = "Ocurrio un error: " + ex.Message;
+                throw;
+            }
+
+
+            return result;
+        }
+    }
+}
